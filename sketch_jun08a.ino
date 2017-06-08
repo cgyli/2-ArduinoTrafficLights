bb7e8da23cbbd72b5db182e97cecf47a05fb99e0
Cathy Li <cgli@sfu.ca>
2017-06-08 00:22:41 -0700

    Sketch Codes for traffic light
    a) basic traffic light functions
    b) with button for pedestrian

---
+++ sketch_jun08a.ino
@@ -0,0 +1,34 @@
+int red = 10;
+int yellow = 9;
+int green = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  changeLights();
+  delay(15000);
+}
+
+void changeLights(){
+  // green lights on
+  digitalWrite(green, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  delay(5000);
+
+  //yellow lights on
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(2000);
+
+  //red lights on
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+}
+
---
+++ sketch_jun08b.ino
@@ -0,0 +1,40 @@
+int red = 10;
+int yellow = 9;
+int green = 8;
+// int button = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  //pinMode(button, INPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*if (digitalRead(button) == (HIGH){
+    delay(15); //software debounce
+    if(digitalRead(button) == (HIGH){
+      */
+  changeLights();
+  delay(15000);
+}
+
+void changeLights(){
+  // green lights on
+  digitalWrite(green, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  delay(5000);
+
+  //yellow lights on
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(2000);
+
+  //red lights on
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+}
+
